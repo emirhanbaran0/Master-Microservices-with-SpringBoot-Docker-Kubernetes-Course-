3f7a872b3f0c37b1412e6b62450a93008af69937
Emirhan Baran <emir.baran255@gmail.com>
2024-06-08 20:35:35 +0300

    Remove all files from "accounts", "cards", and "loans" folders
    This commit corresponds to the full clearance of multiple folders, specifically the "accounts", "cards", and "loans". All files from these directories were removed, including several sets of .gitignore files, various Java classes and resource files. This cleanup is part of a larger restructuring process. loan processing functionalities and tests"
    
    This commit includes the addition of new files and features focused on loan processing. A new `LoanController` provides APIs for creating, fetching, updating, and deleting loan details. Loan details are stored in the `LoansDto` data object and processed through the `LoanService`. Major business logic for managing loans has also been developed. Additionally, JUnit tests for these functionalities are included to ensure correct performance. The involved classes and functionalities are part of a larger financial system implied by the presence of constant files for other financial services like accounts and cards.

---
+++ section4/spingboot-v2/accounts/src/main/java/com/emirhanbaran/accounts/AccountsApplication.java
@@ -0,0 +1,19 @@
+package com.emirhanbaran.accounts;
+
+import com.emirhanbaran.accounts.audit.AuditAwareImpl;
+import com.emirhanbaran.accounts.dto.AccountContactInfoDto;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
+@EnableConfigurationProperties(value = {AccountContactInfoDto.class})
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
