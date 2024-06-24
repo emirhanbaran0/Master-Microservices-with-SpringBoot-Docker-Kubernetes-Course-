063406d4705605ac35bf0bde11bdf81936ede615
Emirhan Baran <emir.baran255@gmail.com>
2024-06-25 00:03:48 +0300

    Add Feign clients and DTOs for Loans and Cards
    Enabled Feign client in the Accounts application and added Feign clients for Loans and Cards. Created DTOs for Loans and Cards to hold the retrieved data. Also, updated the pom.xml to include the Feign dependency and modified the account_number data type in the account table schema.

--- section8/accounts/src/main/java/com/emirhanbaran/accounts/AccountsApplication.java
+++ section8/accounts/src/main/java/com/emirhanbaran/accounts/AccountsApplication.java
@@ -5,11 +5,13 @@ import com.emirhanbaran.accounts.dto.AccountContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 @EnableConfigurationProperties(value = {AccountContactInfoDto.class})
+@EnableFeignClients
 public class AccountsApplication {
 
 	public static void main(String[] args) {
