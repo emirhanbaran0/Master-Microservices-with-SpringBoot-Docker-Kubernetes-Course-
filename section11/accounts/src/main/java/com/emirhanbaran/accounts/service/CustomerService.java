fbc9d4f83ad3f5c40559917afbd3864e0065b055
Emirhan Baran <emir.baran255@gmail.com>
2024-07-23 21:02:02 +0300

    Section 11 is Implemented Section11 related services are initiated.(Logging and Monitoring)

---
+++ section11/accounts/src/main/java/com/emirhanbaran/accounts/service/CustomerService.java
@@ -0,0 +1,58 @@
+package com.emirhanbaran.accounts.service;
+
+import com.emirhanbaran.accounts.dto.AccountsDto;
+import com.emirhanbaran.accounts.dto.CardsDto;
+import com.emirhanbaran.accounts.dto.CustomerDetailsDto;
+import com.emirhanbaran.accounts.dto.LoansDto;
+import com.emirhanbaran.accounts.entity.Account;
+import com.emirhanbaran.accounts.entity.Customer;
+import com.emirhanbaran.accounts.exception.ResourceNotFoundException;
+import com.emirhanbaran.accounts.mapper.AccountsMapper;
+import com.emirhanbaran.accounts.mapper.CustomerMapper;
+import com.emirhanbaran.accounts.repository.AccountRepository;
+import com.emirhanbaran.accounts.repository.CustomerRepository;
+import com.emirhanbaran.accounts.service.client.CardsFeignClient;
+import com.emirhanbaran.accounts.service.client.LoansFeignClient;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import static com.emirhanbaran.accounts.mapper.CustomerMapper.mapToCustomerDetailsDto;
+
+@Service
+@AllArgsConstructor
+public class CustomerService {
+
+    private final AccountRepository accountRepository;
+    private final CustomerRepository customerRepository;
+    private final CardsFeignClient cardsFeignClient;
+    private final LoansFeignClient loansFeignClient;
+
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber,String correlationId) {
+        Customer customer=customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                ()->{throw new ResourceNotFoundException("Customer","mobileNumber",mobileNumber);}
+        );
+        Account account=accountRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                ()->{throw new ResourceNotFoundException("Account", "customerId",customer.getCustomerId().toString());}
+        );
+        CustomerDetailsDto customerDetailsDto=CustomerMapper.mapToCustomerDetailsDto(customer,new CustomerDetailsDto());
+        customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(account,new AccountsDto()));
+        ResponseEntity<CardsDto> cardsResponse = cardsFeignClient.fetchCardDetails(correlationId,mobileNumber);
+        if (cardsResponse.getStatusCode() == HttpStatus.OK) {
+            customerDetailsDto.setCardsDto(cardsResponse.getBody());
+        }
+        ResponseEntity<LoansDto> loansResponse = loansFeignClient.fetchLoanDetails(correlationId,mobileNumber);
+        if (loansResponse.getStatusCode() == HttpStatus.OK) {
+            customerDetailsDto.setLoansDto(loansResponse.getBody());
+        }
+            return customerDetailsDto;
+    }
+}
---
+++ section11/loans/src/main/java/com/emirhanbaran/loans/controller/LoansController.java
@@ -0,0 +1,196 @@
+package com.emirhanbaran.loans.controller;
+
+
+import com.emirhanbaran.loans.constants.LoansConstants;
+import com.emirhanbaran.loans.dto.ErrorResponseDto;
+import com.emirhanbaran.loans.dto.LoansDto;
+import com.emirhanbaran.loans.dto.LoanConfigInfoDto;
+import com.emirhanbaran.loans.dto.ResponseDto;
+import com.emirhanbaran.loans.service.LoanService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author Eazy Bytes
+ */
+
+@Tag(
+        name = "CRUD REST APIs for Loans in EazyBank",
+        description = "CRUD REST APIs in EazyBank to CREATE, UPDATE, FETCH AND DELETE loan details"
+)
+@RestController
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@RequiredArgsConstructor
+@Validated
+public class LoansController {
+
+    private static final Logger logger=LoggerFactory.getLogger(LoansController.class);
+    private final LoanService loanService;
+    @Value("${build.version}")
+    private String buildVersion;
+    private final Environment environment;
+    private final LoanConfigInfoDto loginConfigInfoDto;
+
+    @Operation(
+            summary = "Create Loan REST API",
+            description = "REST API to create new loan inside EazyBank"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status CREATED"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createLoan(@RequestParam
+                                                  @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+                                                  String mobileNumber) {
+        loanService.createLoan(mobileNumber);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(LoansConstants.STATUS_201, LoansConstants.MESSAGE_201));
+    }
+
+    @Operation(
+            summary = "Fetch Loan Details REST API",
+            description = "REST API to fetch loan details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/fetch")
+    public ResponseEntity<LoansDto> fetchLoanDetails(@RequestHeader("eazybank-correlation-id") String correlationId,
+                                                     @RequestParam
+                                                     @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+                                                     String mobileNumber) {
+        logger.debug("Loans - eazyBank Correlation Id: " + correlationId);
+        LoansDto loansDto = loanService.fetchLoan(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(loansDto);
+    }
+
+    @Operation(
+            summary = "Update Loan Details REST API",
+            description = "REST API to update loan details based on a loan number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateLoanDetails(@Valid @RequestBody LoansDto loansDto) {
+        boolean isUpdated = loanService.updateLoan(loansDto);
+        if(isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(LoansConstants.STATUS_200, LoansConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(LoansConstants.STATUS_417, LoansConstants.MESSAGE_417_UPDATE));
+        }
+    }
+
+    @Operation(
+            summary = "Delete Loan Details REST API",
+            description = "REST API to delete Loan details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDto> deleteLoanDetails(@RequestParam
+                                                         @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+                                                         String mobileNumber) {
+        boolean isDeleted = loanService.deleteLoan(mobileNumber);
+        if(isDeleted) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(LoansConstants.STATUS_200, LoansConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(LoansConstants.STATUS_417, LoansConstants.MESSAGE_417_DELETE));
+        }
+    }
+
+    @GetMapping("build-info")
+    public ResponseEntity<String> getBuildInfo() {
+        return  ResponseEntity.status(HttpStatus.OK).body(buildVersion);
+    }
+
+    @GetMapping("java-info")
+    public ResponseEntity<String> getJavaInfo() {
+        return  ResponseEntity.status(HttpStatus.OK).body(environment.getProperty("JAVA_HOME"));
+    }
+
+    @GetMapping("contact-info")
+    public ResponseEntity<LoanConfigInfoDto> getContactInfo(){
+        return ResponseEntity.status(HttpStatus.OK).body(loginConfigInfoDto);
+    }
+
+
+}
